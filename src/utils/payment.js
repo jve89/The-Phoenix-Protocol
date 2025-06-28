831eff8e577cd791d6ce322749297334a5b74450
jve89 <johanvanerkel@gmail.com>
2025-06-28 20:31:20 +0000

    Final Stripe test

--- src/utils/payment.js
+++ src/utils/payment.js
@@ -1,4 +1,5 @@
 const Stripe = require('stripe');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2023-10-16' });
 
 const createCheckoutSession = async (email, plan) => {
   if (!process.env.STRIPE_SECRET_KEY) {
