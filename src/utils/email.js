677eef7831edd1380e2403a8e407855c5bbcc451
jve89 <johanvanerkel@gmail.com>
2025-06-29 18:05:31 +0000

    Added Procfile for Heroku auto-start

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,14 +2,15 @@ const sgMail = require('@sendgrid/mail');
 require('dotenv').config({ path: './.env' });
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const fromEmail = 'no-reply@thephoenixprotocol.app'; // Real sender
 
 const sendEmail = async (to, subject, html) => {
-  const msg = { to, from: 'no-reply@thephoenixprotocol.com', subject, html };
+  const msg = { to, from: fromEmail, subject, html };
   try {
     await sgMail.send(msg);
-    console.log('Email sent to', to);
+    console.log('Email sent to', to, 'from', fromEmail);
   } catch (error) {
-    console.error('Email error:', error);
+    console.error('Email error:', error.response ? error.response.body : error.message);
   }
 };
 
