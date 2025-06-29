ffdc40e9c89b7c0a9c1f74b96f328b48695da588
jve89 <johanvanerkel@gmail.com>
2025-06-29 15:34:06 +0000

    Resolved SSL and DNS issues

--- src/server.js
+++ src/server.js
@@ -6,6 +6,14 @@ dotenv.config({ path: './.env' }); // Explicit root path
 if (!process.env.STRIPE_SECRET_KEY) console.error('Stripe key not loaded');
 
 const app = express();
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
